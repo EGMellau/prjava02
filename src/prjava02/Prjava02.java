7bff198e4cfb2b6f485deb5b89078ea27e1cae0a
M8 <ergrme1516daw@gmail.com>
2016-04-19 21:08:23 +0200

    Branca 01?

--- src/prjava02/Prjava02.java
+++ src/prjava02/Prjava02.java
@@ -20,6 +20,7 @@ public static void main(String[] args) {
 		String hostname = adreça.getHostName();
 		System.out.println("Creació d'una branca del projecte prjava02");
 		System.out.println("Afegint més codi a la branca00 del projecte prjava02");
+		System.out.println("Afegint codi a la branca01 del projecte prjava02");
 		System.out.println("hostname="+hostname);
 		System.out.println("Nom de l'usuari: " + System.getProperty("user.name"));
 		System.out.println("Carpeta Personal: " + System.getProperty("user.home"));
